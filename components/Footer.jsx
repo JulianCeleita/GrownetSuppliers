081586c20f7c50041681431075d9a6cb1cb16db5
liamgabriel2021 <arbey221@gmail.com>
2023-11-21 16:46:45 -0500

    classname footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -4,12 +4,12 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <footer class="bg-white rounded-lg shadow dark:bg-gray-900 m-4">
-      <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
-        <div class="sm:flex sm:items-center sm:justify-between">
+    <footer className="bg-white rounded-lg shadow dark:bg-gray-900 m-4">
+      <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
+        <div className="sm:flex sm:items-center sm:justify-between">
           <a
             href="https://flowbite.com/"
-            class="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse"
+            className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse"
           >
             <Link
               href="#"
@@ -23,7 +23,7 @@ export default function Footer() {
               />
             </Link>
           </a>
-          <ul class="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
+          <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
             <li>
               <a href="#" class="hover:underline me-4 md:me-6">
                 About
@@ -46,10 +46,10 @@ export default function Footer() {
             </li>
           </ul>
         </div>
-        <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-        <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
+        <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+        <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
           © 2023{" "}
-          <a href="" class="hover:underline">
+          <a href="" className="hover:underline">
             Grownet™
           </a>
           . All Rights Reserved.
