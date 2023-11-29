9f894393e2a9c4c8be0e6754a8afb50de919569d
liamgabriel2021 <arbey221@gmail.com>
2023-11-29 15:37:55 -0500

    fix axios

--- app/components/NewCategory.jsx
+++ app/components/NewCategory.jsx
@@ -2,7 +2,7 @@ import { addCategoryUrl } from "@/app/config/urls.config";
 import { XMarkIcon } from "@heroicons/react/24/outline";
 import axios from "axios";
 import { useState } from "react";
-import useTokenStore from "@/store/useTokenStore";
+import useTokenStore from "@/app/store/useTokenStore";
 
 function NewCategory({ isvisible, onClose, setCategories }) {
   if (!isvisible) {
