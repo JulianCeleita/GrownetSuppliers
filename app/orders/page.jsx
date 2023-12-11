a11c0710043c7d64d1f19a3499968dc374847190
pvgmoon <pandreavanegasg@gmail.com>
2023-12-11 10:39:26 -0500

    input row detalles de la orden

--- app/orders/page.jsx
+++ app/orders/page.jsx
@@ -153,22 +153,41 @@ const OrderView = () => {
           </div>
         </div>
         <div className="bg-white p-5 pr-9 pl-9 rounded-lg">
-          <div className="flex">
-            <div className="flex flex-col w-[50%]">
+          <div className="flex flex-col">
+            <div className="flex items-center mb-3 bg-yellow-200 w-[100%]">
+              <label>Date: </label>
+              <input type="date" className="border ml-2 p-1.5 rounded-md" />
+              <label>Inv. No.: </label>
+              <input type="text" className="border ml-2 p-1.5 rounded-md" />
+            </div>
+            <div className="flex items-center mb-3 bg-red-200 w-[100%]">
+              <label>A/C: </label>
+              <input
+                type="text"
+                className="border ml-2 p-1.5 rounded-md mr-2"
+              />
+              <label>Order No.: </label>
+              <input
+                type="text"
+                className="border ml-2 p-1.5 rounded-md mr-2"
+              />
+            </div>
+            {/* <div className="flex flex-col w-[50%]">
               <label>Date: </label>
               <input type="date" className="border p-1.5 rounded-md mr-2" />
-              <label className="mt-2">A/C: </label>
-              <input type="text" className="border p-1.5 rounded-md mr-2" />
+              
             </div>
             <div className="flex flex-col w-[50%]">
               <label>Inv. No.: </label>
               <input type="text" className="border p-1.5 rounded-md mr-2" />
               <label className="mt-2">Order No.: </label>
               <input type="text" className="border p-1.5 rounded-md mr-2" />
-            </div>
+              </div>*/}
           </div>
-          <label>Customer: </label>
-          <input type="text" className="border p-3 rounded-md mt-3 w-full" />
+          <div className="flex items-center bg-blue-400">
+            <label>Customer: </label>
+            <input type="text" className="border p-2 rounded-md ml-5 w-full" />
+          </div>
         </div>
       </div>
       <div className="-mt-20">
