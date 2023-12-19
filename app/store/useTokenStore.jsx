f864be56ed65dc44df2a3e1ea3375f19502083b2
Julian Celeita <andresceleita@yahoo.es>
2023-12-19 14:36:07 -0500

    Cambios en token y links de QA

--- app/store/useTokenStore.jsx
+++ app/store/useTokenStore.jsx
@@ -1,9 +1,11 @@
 import { create } from "zustand";
 
 const useTokenStore = create(() => ({
-  token: "1322|D0wbgSlhEIZthyIl9gsH4YSVqw5mowyrkldqHFhF",
+  // NUEVO TOKEN
+  token: "1846|frQSf3ZxPsiwEUfx5INSMvsjQcJhScLy6L0B1Tna",
 }));
 
 export default useTokenStore;
 
+// TOKEN ORIGINAL
 // 1322|D0wbgSlhEIZthyIl9gsH4YSVqw5mowyrkldqHFhF
