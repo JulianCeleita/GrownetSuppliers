de91a6dcde25db71e6c20a03f986b2700b3ffd5d
Santiago Arango <arangosanti05@gmail.com>
2023-12-28 17:44:13 -0500

    insercion de urls y correcion de gitignore

---
+++ app/config/urls.config.js
@@ -0,0 +1,66 @@
+// Products
+export const productsUrl =
+  "https://api.grownetapp.com/grownet/api/products/all";
+export const addProductUrl =
+  "https://api.grownetapp.com/grownet/api/products/create";
+export const updateProductUrl =
+  "https://api.grownetapp.com/grownet/api/products/update/";
+export const deleteProductUrl =
+  "https://api.grownetapp.com/grownet/api/products/delete/";
+
+//Presentations
+export const presentationsUrl =
+  "https://api.grownetapp.com/grownet/api/presentations/all";
+export const addPresentationUrl =
+  "https://api.grownetapp.com/grownet/api/presentations/create";
+export const updatePresentationUrl =
+  "https://api.grownetapp.com/grownet/api/presentations/update/";
+export const deletePresentationUrl =
+  "https://api.grownetapp.com/grownet/api/presentations/delete/";
+
+//Categories
+export const categoriesUrl =
+  "https://api.grownetapp.com/grownet/api/categories/all";
+export const addCategoryUrl =
+  "https://api.grownetapp.com/grownet/api/categories/created";
+export const updateCategoryUrl =
+  "https://api.grownetapp.com/grownet/api/categories/updated/";
+export const deleteCategoryUrl =
+  "https://api.grownetapp.com/grownet/api/categories/delete/";
+
+//Suppliers
+export const suppliersUrl =
+  "https://api.grownetapp.com/grownet/api/suppliers/all";
+export const addSupplierUrl =
+  "https://api.grownetapp.com/grownet/api/suppliers/create";
+export const updateSupplierUrl =
+  "https://api.grownetapp.com/grownet/api/suppliers/update/";
+export const deleteSupplierUrl =
+  "https://api.grownetapp.com/grownet/api/suppliers/delete/";
+
+//Families
+export const familiesUrl =
+  "https://api.grownetapp.com/grownet/api/families/all";
+
+//UOM
+export const uomUrl = "https://api.grownetapp.com/grownet/api/uom/all";
+
+//Taxes
+export const taxexUrl = "https://api.grownetapp.com/grownet/api/taxes/all";
+
+//Orders
+export const restaurantsData =
+  "https://api.grownetapp.com/grownet/api/customers/all";
+export const createOrders =
+  "https://api.grownetapp.com/grownet/api/orders/create";
+
+export const customersData =
+  "https://api.grownetapp.com/grownet/api/customers/serch/";
+
+export const presentationsCode =
+  "https://api.grownetapp.com/grownet/api/presentations/search/";
+
+export const presentationData =
+  "https://api.grownetapp.com/grownet/api/products/presentations/all";
+export const createStorageOrder =
+  "https://api.grownetapp.com/grownet/api/orders/create";
