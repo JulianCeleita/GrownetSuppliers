b2af702ded76defb8642d1f4f70a005af0e5384d
Santiago Arango <arangosanti05@gmail.com>
2023-12-28 11:44:11 -0500

    feat: ajustes visuales interfaz login

--- app/components/LoginForm.jsx
+++ app/components/LoginForm.jsx
@@ -1,44 +1,66 @@
-import React from "react";
+import Image from "next/image";
+import { useState } from "react";
+import { useRouter } from "next/router";
+import logo_blancov2 from "../img/logo_blancov2.svg";
 
-export default function LoginForm({
-  username,
-  setUsername,
-  password,
-  setPassword,
-  handleLogin,
-}) {
-  return (
-    <form>
-      <div className="mb-3 flex justify-between">
-        <label>Email:</label>
-        <input
-          className="border border-inherit rounded-sm px-2"
-          type="text"
-          placeholder="email@user.com"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-        />
-      </div>
+function LoginForm() {
+    // const router = useRouter();
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
 
-      <div className="mb-3 flex justify-between gap-2">
-        <label>Password:</label>
-        <input
-          className="border border-inherit rounded-sm px-2"
-          type="password"
-          placeholder="**************"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-      </div>
-      <div className="mt-5 flex justify-center items-center">
-        <button
-          className="bg-green hover:bg-light-green text-black transition-all font-bold py-2 px-4 rounded w-80"
-          type="button"
-          onClick={handleLogin}
-        >
-          Log in
-        </button>
-      </div>
-    </form>
-  );
+    const handleLogin = () => {
+        // TODO cuando se tenga la lógica de iniciar sesion se realiza acá
+        console.log('Usuario:', username, 'Contraseña:', password);
+        // router.push("/orders");
+    };
+
+    return (
+        <body>
+            <div className="min-h-screen flex flex-col items-center justify-center bg-blue-500">
+                <div className="p-8 flex flex-col items-center">
+                    <Image
+                        src={logo_blancov2}
+                        alt="logo"
+                        width={250}
+                        height={250}
+                    />
+                    <form className="mt-5">
+                        <div className="mb-3 flex flex-col items-center">
+                            <label className="text-white">
+                                Email:
+                            </label>
+                            <input
+                                className="border border-inherit rounded-3xl p-3 w-[100%]"
+                                type="text"
+                                placeholder="email@user.com"
+                                value={username}
+                                onChange={(e) => setUsername(e.target.value)}
+                            />
+                        </div>
+
+                        <div className="mb-3 flex flex-col items-center">
+                            <label className="text-white">
+                                Password:
+                            </label>
+                            <input
+                                className="border border-inherit rounded-3xl p-3 w-[100%]"
+                                type="password"
+                                placeholder="**************"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                            />
+                        </div>
+                        <div className="mt-5 flex justify-center items-center">
+                            <button className="bg-dark-blue hover:scale-110 text-white transition-all font-bold py-2 px-4 rounded-xl w-80" onClick={handleLogin}>
+                                Log in
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </body>
+    );
 }
+
+
+export default LoginForm;
