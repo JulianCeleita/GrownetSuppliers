5b7ba9735a0097222387a21ccfb196f497497b76
Julian Celeita <andresceleita@yahoo.es>
2023-12-29 10:08:50 -0500

    Se suben los endpoints de nuevo

---
+++ app/config/urls.config.js
@@ -0,0 +1,66 @@
+// Products
+export const productsUrl =
+"http://3.139.16.141/api/products/all";
+export const addProductUrl =
+"http://3.139.16.141/api/products/create";
+export const updateProductUrl =
+"http://3.139.16.141/api/products/update/";
+export const deleteProductUrl =
+"http://3.139.16.141/api/products/delete/";
+
+//Presentations
+export const presentationsUrl =
+  "http://3.139.16.141/api/presentations/all";
+export const addPresentationUrl =
+  "http://3.139.16.141/api/presentations/create";
+export const updatePresentationUrl =
+  "http://3.139.16.141/api/presentations/update/";
+export const deletePresentationUrl =
+  "http://3.139.16.141/api/presentations/delete/";
+
+//Categories
+export const categoriesUrl =
+  "http://3.139.16.141/api/categories/all";
+export const addCategoryUrl =
+  "http://3.139.16.141/api/categories/created";
+export const updateCategoryUrl =
+  "http://3.139.16.141/api/categories/updated/";
+export const deleteCategoryUrl =
+  "http://3.139.16.141/api/categories/delete/";
+
+//Suppliers
+export const suppliersUrl =
+  "http://3.139.16.141/api/suppliers/all";
+export const addSupplierUrl =
+  "http://3.139.16.141/api/suppliers/create";
+export const updateSupplierUrl =
+  "http://3.139.16.141/api/suppliers/update/";
+export const deleteSupplierUrl =
+  "http://3.139.16.141/api/suppliers/delete/";
+
+//Families
+export const familiesUrl =
+  "http://3.139.16.141/api/families/all";
+
+//UOM
+export const uomUrl = "http://3.139.16.141/api/uom/all";
+
+//Taxes
+export const taxexUrl = "http://3.139.16.141/api/taxes/all";
+
+//Orders
+export const restaurantsData =
+  "http://3.139.16.141/api/customers/all";
+export const createOrders =
+  "http://3.139.16.141/api/orders/create";
+
+export const customersData =
+  "http://3.139.16.141/api/customers/serch/";
+
+export const presentationsCode =
+  "http://3.139.16.141/api/presentations/search/";
+
+export const presentationData =
+  "http://3.139.16.141/api/products/presentations/all";
+export const createStorageOrder =
+  "http://3.139.16.141/api/orders/create";
