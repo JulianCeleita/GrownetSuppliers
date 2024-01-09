d89ebf432f68d02a716b06a3cfd00465b4126bf8
Santiago Arango <arangosanti05@gmail.com>
2024-01-09 12:29:08 -0500

    cambio de urls y agrego prevent default en la tabla de ordenes

--- app/config/urls.config.js
+++ app/config/urls.config.js
@@ -1,82 +1,84 @@
 // login
 export const loginUrl =
-"http://3.139.16.141/api/login/login";
+"https://api.grownetapp.com/grownet/api/login/login";
 
 // users
 export const usersUrl =
-"http://3.139.16.141/api/user/all";
+"https://api.grownetapp.com/grownet/api/user/all";
 
 // Products
 export const productsUrl =
-"http://3.139.16.141/api/products/all";
+"https://api.grownetapp.com/grownet/api/products/all";
 export const addProductUrl =
-"http://3.139.16.141/api/products/create";
+"https://api.grownetapp.com/grownet/api/products/create";
 export const updateProductUrl =
-"http://3.139.16.141/api/products/update/";
+"https://api.grownetapp.com/grownet/api/products/update/";
 export const deleteProductUrl =
-"http://3.139.16.141/api/products/delete/";
+"https://api.grownetapp.com/grownet/api/products/delete/";
 
 //Presentations
 export const presentationsUrl =
-  "http://3.139.16.141/api/presentations/all";
+  "https://api.grownetapp.com/grownet/api/presentations/all";
   export const presentationsSupplierUrl =
-  "http://3.139.16.141/api/presentations/supplier/";
+  "https://api.grownetapp.com/grownet/api/presentations/supplier/";
 export const addPresentationUrl =
-  "http://3.139.16.141/api/presentations/create";
+  "https://api.grownetapp.com/grownet/api/presentations/create";
 export const updatePresentationUrl =
-  "http://3.139.16.141/api/presentations/update/";
+  "https://api.grownetapp.com/grownet/api/presentations/update/";
 export const deletePresentationUrl =
-  "http://3.139.16.141/api/presentations/delete/";
+  "https://api.grownetapp.com/grownet/api/presentations/delete/";
 
 //Categories
 export const categoriesUrl =
-  "http://3.139.16.141/api/categories/all";
+  "https://api.grownetapp.com/grownet/api/categories/all";
 export const addCategoryUrl =
-  "http://3.139.16.141/api/categories/created";
+  "https://api.grownetapp.com/grownet/api/categories/created";
 export const updateCategoryUrl =
-  "http://3.139.16.141/api/categories/updated/";
+  "https://api.grownetapp.com/grownet/api/categories/updated/";
 export const deleteCategoryUrl =
-  "http://3.139.16.141/api/categories/delete/";
+  "https://api.grownetapp.com/grownet/api/categories/delete/";
 
 //Suppliers
 export const suppliersUrl =
-  "http://3.139.16.141/api/suppliers/all";
+  "https://api.grownetapp.com/grownet/api/suppliers/all";
 export const addSupplierUrl =
-  "http://3.139.16.141/api/suppliers/create";
+  "https://api.grownetapp.com/grownet/api/suppliers/create";
 export const updateSupplierUrl =
-  "http://3.139.16.141/api/suppliers/update/";
+  "https://api.grownetapp.com/grownet/api/suppliers/update/";
 export const deleteSupplierUrl =
-  "http://3.139.16.141/api/suppliers/delete/";
+  "https://api.grownetapp.com/grownet/api/suppliers/delete/";
 
 //Families
 export const familiesUrl =
-  "http://3.139.16.141/api/families/all";
+  "https://api.grownetapp.com/grownet/api/families/all";
 
 //UOM
-export const uomUrl = "http://3.139.16.141/api/uom/all";
+export const uomUrl = "https://api.grownetapp.com/grownet/api/uom/all";
 
 //Taxes
-export const taxexUrl = "http://3.139.16.141/api/taxes/all";
+export const taxexUrl = "https://api.grownetapp.com/grownet/api/taxes/all";
 
 //Orders
 export const ordersUrl = 
-  "http://3.139.16.141/api/orders/all";
+  "https://api.grownetapp.com/grownet/api/orders/all";
 export const ordersSupplierUrl = 
-  "http://3.139.16.141/api/orders/supplier/";
+  "https://api.grownetapp.com/grownet/api/orders/supplier/";
 export const orderDetail = 
-  "http://3.139.16.141/api/orders/";
+  "https://api.grownetapp.com/grownet/api/orders/";
 export const restaurantsData =
-  "http://3.139.16.141/api/customers/all";
+  "https://api.grownetapp.com/grownet/api/customers/all";
 export const createOrders =
-  "http://3.139.16.141/api/orders/create";
+  "https://api.grownetapp.com/grownet/api/orders/create";
 
 export const customersData =
-  "http://3.139.16.141/api/customers/serch/";
+  "https://api.grownetapp.com/grownet/api/customers/serch/";
 
 export const presentationsCode =
-  "http://3.139.16.141/api/presentations/search/";
+  "https://api.grownetapp.com/grownet/api/presentations/search/";
 
 export const presentationData =
-  "http://3.139.16.141/api/products/presentations/all";
+  "https://api.grownetapp.com/grownet/api/products/presentations/all";
 export const createStorageOrder =
-  "http://3.139.16.141/api/orders/create";
+  "https://api.grownetapp.com/grownet/api/orders/create";
+
+  // http://3.139.16.141
