a942b8df57e782d275597367bc5c6f2b4a8d52b4
liamgabriel2021 <arbey221@gmail.com>
2024-01-10 19:50:31 -0500

    next config restablecido

--- next.config.js
+++ next.config.js
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  trailingSlash: true,
-  exportPathMap: async function (defaultPathMap) {
-    return {
-      "/": { page: "/" },
-      "/pages/order/[orderId]/": { page: "/pages/order/[orderId]" },
-      // Agrega aquí todas las demás rutas que necesites
-    };
-  },
+  output: "export",
 };
 
 module.exports = nextConfig;
