0eb6f1854dc3504342261f9f9e843ae07af34a44
liamgabriel2021 <arbey221@gmail.com>
2024-01-10 19:46:48 -0500

    prueba 6.2 enrutado d

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
+  trailingSlash: true,
   exportPathMap: async function (defaultPathMap) {
     return {
       "/": { page: "/" },
-      "/pages/order/[orderId]": { page: "/pages/order/[orderId]" },
+      "/pages/order/[orderId]/": { page: "/pages/order/[orderId]" },
       // Agrega aquí todas las demás rutas que necesites
     };
   },
