1e7be40bc79e886a82f471142156dbbcd2bbe712
Johny Villegas <55726826+JohnyVB@users.noreply.github.com>
2024-02-07 13:35:57 -0500

    Funcion de seleccionar ordenes para imprimir lista

--- app/orders/page.jsx
+++ app/orders/page.jsx
@@ -18,7 +18,7 @@ const formatDate = (dateString) => {
   const formattedDate = format(new Date(dateString), "yyyy-MM-dd");
   return formattedDate;
 };
-// TODO: revisar por qué se dañó la filtracion por fechas y calendario
+// TODO: revisar por qué se dañó la filtración por fechas y calendario
 const OrderView = () => {
   const router = useRouter();
   const { token } = useTokenStore();
@@ -37,6 +37,8 @@ const OrderView = () => {
   const [dayAfterTomorrow, setdayAfterTomorrow] = useState(
     formatDate(new Date(new Date().getTime() + 2 * 24 * 60 * 60 * 1000))
   );
+
+  const [selectedOrders, setSelectedOrders] = useState({});
 
   const todayOrdersCount = countOrdersForDate(orders, "today");
   const tomorrowOrdersCount = countOrdersForDate(orders, "tomorrow");
@@ -64,6 +66,7 @@ const OrderView = () => {
       document.removeEventListener("mousedown", handleOutsideClick);
     };
   }, [user, token, showDatePicker]);
+
 
   const filterOrdersByDate = (order) => {
     if (showAllOrders) {
@@ -103,6 +106,37 @@ const OrderView = () => {
     setSelectedDate(date);
   };
 
+  const goToOrder = (e, order) => {
+    e.preventDefault();
+    router.push(`/order/${order.reference}`, undefined, {
+      shallow: true,
+    });
+  }
+
+  const handleOrderSelect = (order, checked) => {
+    setSelectedOrders(prevState => ({
+      ...prevState,
+      [order.reference]: checked
+    }));
+  };
+
+  const selectAll = (checked) => {
+    const newSelectedOrders = {};
+    sortedOrders.forEach((order) => {
+      newSelectedOrders[order.reference] = checked;
+    });
+    setSelectedOrders(newSelectedOrders);
+  };
+
+  const printOrders = () => {
+    const ordersToPrint = Object.entries(selectedOrders)
+      .filter(([reference, checked]) => checked)
+      .map(([reference]) => reference);
+
+    console.log('ordersToPrint', ordersToPrint);
+    //TODO: implementar lógica para imprimir las ordenes seleccionadas
+  }
+
   const totalOrders = orders.length;
 
   const filteredOrders = orders.filter((order) => filterOrdersByDate(order));
@@ -129,9 +163,8 @@ const OrderView = () => {
         </div>
         <div className="flex items-center justify-center space-x-4">
           <button
-            className={`text-dark-blue border-b-2 border-stone-100 cursor-pointer rounded-xl p-1 ${
-              showAllOrders ? "font-semibold bg-blue-300 transition-all" : ""
-            }`}
+            className={`text-dark-blue border-b-2 border-stone-100 cursor-pointer rounded-xl p-1 ${showAllOrders ? "font-semibold bg-blue-300 transition-all" : ""
+              }`}
             onClick={() => {
               setDateFilter("");
               setShowAllOrders(true);
@@ -140,11 +173,10 @@ const OrderView = () => {
             All ({totalOrders})
           </button>
           <button
-            className={`text-dark-blue border-b-2 border-stone-100 cursor-pointer rounded-xl p-1 ${
-              dateFilter === "today"
-                ? "font-semibold bg-blue-300  transition-all"
-                : ""
-            }`}
+            className={`text-dark-blue border-b-2 border-stone-100 cursor-pointer rounded-xl p-1 ${dateFilter === "today"
+              ? "font-semibold bg-blue-300  transition-all"
+              : ""
+              }`}
             onClick={() => {
               const currentDate = new Date();
               const today = new Date(currentDate);
@@ -157,11 +189,10 @@ const OrderView = () => {
             {today} ({todayOrdersCount})
           </button>
           <button
-            className={`text-dark-blue border-b-2 border-stone-100 cursor-pointer rounded-xl p-1 ${
-              dateFilter === "tomorrow"
-                ? "font-semibold bg-blue-300 transition-all"
-                : ""
-            }`}
+            className={`text-dark-blue border-b-2 border-stone-100 cursor-pointer rounded-xl p-1 ${dateFilter === "tomorrow"
+              ? "font-semibold bg-blue-300 transition-all"
+              : ""
+              }`}
             onClick={() => {
               const currentDate = new Date();
               const tomorrow = new Date(currentDate);
@@ -174,11 +205,10 @@ const OrderView = () => {
             {tomorrow} ({tomorrowOrdersCount})
           </button>
           <button
-            className={`text-dark-blue border-b-2 border-stone-100 cursor-pointer rounded-xl p-1 ${
-              dateFilter === "dayAfterTomorrow"
-                ? "font-semibold bg-blue-300 transition-all"
-                : ""
-            }`}
+            className={`text-dark-blue border-b-2 border-stone-100 cursor-pointer rounded-xl p-1 ${dateFilter === "dayAfterTomorrow"
+              ? "font-semibold bg-blue-300 transition-all"
+              : ""
+              }`}
             onClick={() => {
               const currentDate = new Date();
               const dayAfterTomorrow = new Date(currentDate);
@@ -191,9 +221,8 @@ const OrderView = () => {
             {dayAfterTomorrow} ({dayAfterTomorrowOrdersCount})
           </button>
           <button
-            className={`text-dark-blue border-b-2 border-stone-100 cursor-pointer rounded-xl p-1 ${
-              showDatePicker ? "disabled" : ""
-            }`}
+            className={`text-dark-blue border-b-2 border-stone-100 cursor-pointer rounded-xl p-1 ${showDatePicker ? "disabled" : ""
+              }`}
             onClick={() => {
               setShowDatePicker(!showDatePicker);
               setDateFilter("calendarDate");
@@ -201,6 +230,13 @@ const OrderView = () => {
             disabled={showDatePicker}
           >
             +
+          </button>
+
+          <button
+            className="text-white bg-primary-blue border-b-2 border-stone-100 cursor-pointer rounded-xl px-5"
+            onClick={() => printOrders()}
+          >
+            Print
           </button>
 
           {showDatePicker && (
@@ -217,7 +253,17 @@ const OrderView = () => {
           <table className="w-[90%] bg-white rounded-2xl text-center shadow-[0_8px_30px_rgb(0,0,0,0.12)]">
             <thead className="sticky top-0 bg-white shadow-[0px_11px_15px_-3px_#edf2f7] ">
               <tr className="border-b-2 border-stone-100 text-dark-blue">
-                <th className="py-4 rounded-tl-lg">Customer</th>
+                <th className="py-4 rounded-tl-lg flex items-center justify-center">
+                  Select all
+                  <label className="inline-flex items-center ml-3">
+                    <input
+                      type="checkbox"
+                      className="form-checkbox h-5 w-5 text-blue-500"
+                      onChange={(e) => selectAll(e.target.checked)}
+                    />
+                  </label>
+                </th>
+                <th className="py-4">Customer</th>
                 <th className="py-4">Order date</th>
                 <th className="py-4">Delivery date</th>
                 <th className="py-4">Order status</th>
@@ -227,15 +273,24 @@ const OrderView = () => {
               {sortedOrders.map((order, index) => (
                 <tr
                   key={index}
-                  className="text-dark-blue border-b-2 border-stone-100 cursor-pointer"
-                  onClick={(e) => {
-                    e.preventDefault();
-                    router.push(`/order/${order.reference}`, undefined, {
-                      shallow: true,
-                    });
-                  }}
+                  className="text-dark-blue border-b-2 border-stone-100"
                 >
-                  <td className="py-4">{order.accountName}</td>
+                  <td className="py-4">
+                    <label className="inline-flex items-center">
+                      <input
+                        type="checkbox"
+                        className="form-checkbox h-5 w-5 text-blue-500"
+                        checked={!!selectedOrders[order.reference]}
+                        onChange={(e) => handleOrderSelect(order, e.target.checked)}
+                      />
+                    </label>
+                  </td>
+                  <td
+                    className="py-4 cursor-pointer hover:bg-primary-blue hover:text-white"
+                    onClick={(e) => goToOrder(e, order)}
+                  >
+                    {order.accountName}
+                  </td>
                   <td className="py-4">{formatDate(order.created_date)}</td>
                   <td className="py-4">{order.date_delivery}</td>
                   <td className="py-4">{order.name_status}</td>
@@ -250,7 +305,7 @@ const OrderView = () => {
           </div>
         )}
       </div>
-    </Layout>
+    </Layout >
   );
 };
 export default OrderView;
