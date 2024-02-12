392978b36c3ff7a1027a173f75fa9cbb403521ca
Julian Celeita <andresceleita@yahoo.es>
2024-02-12 12:40:02 -0500

    Cambio de nombre a los campos

--- app/order/[orderId]/page.jsx
+++ app/order/[orderId]/page.jsx
@@ -393,7 +393,7 @@ const OrderDetailPage = () => {
           readOnly
           className="border ml-2 p-1.5 rounded-md w-20"
         />
-        <label className="mx-3">Order Number: </label>
+        <label className="mx-3">Customer Ref: </label>
         <input type="text" className="border p-2 rounded-md w-20" />
         {details ? (
           <button
