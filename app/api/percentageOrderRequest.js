08c8bb05418fa579a8a914a4a2cf6436bd9ff2b2
Santiago Arango <arangosanti05@gmail.com>
2024-02-15 09:59:37 -0500

    forzado para renderizacion de detalle de orden

--- app/api/percentageOrderRequest.js
+++ app/api/percentageOrderRequest.js
@@ -14,12 +14,17 @@ export const getPercentageOrder = async (token, date, order_id, setPercentage, s
             }
         );
 
+        console.log(data);
+        setDataLoaded(true);
+
         data.routes.forEach((route) => {
             route.accounts.forEach((account) => {
+                console.log("order reference", account.orders_reference);
+                console.log("order reference", order_id);
                 if (account.orders_reference === Number(order_id)) {
                     const percentage = Number(account.percentage_loading).toFixed(0);
                     setPercentage(percentage);
-                    setDataLoaded(true);
+                    console.log("holaaaaa");
                 }
             });
         });
