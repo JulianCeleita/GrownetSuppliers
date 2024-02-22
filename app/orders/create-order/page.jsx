181216e8784ff698b116f097c5d1ddd0df233c5c
liamgabriel2021 <arbey221@gmail.com>
2024-02-22 09:42:44 -0500

    order date a fecha actual

--- app/orders/create-order/page.jsx
+++ app/orders/create-order/page.jsx
@@ -48,7 +48,6 @@ const CreateOrderView = () => {
   //Fecha input
   function getCurrentDate() {
     const today = new Date();
-    today.setDate(today.getDate() + 1);
     const year = today.getFullYear();
     const month = String(today.getMonth() + 1).padStart(2, "0");
     const day = String(today.getDate()).padStart(2, "0");
