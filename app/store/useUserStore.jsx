2b2e1a45a59ff659525495c85bd28dc0e93f26ac
Santiago Arango <arangosanti05@gmail.com>
2024-02-29 12:04:57 -0500

    se actualiza de nuevo el store del user y el layout

--- app/store/useUserStore.jsx
+++ app/store/useUserStore.jsx
@@ -1,47 +1,24 @@
 import { create } from "zustand";
-import { arrayBufferToBase64, base64ToArrayBuffer, decryptData, encryptData, getKey } from "../utils/cryptoUtils";
 
 const useUserStore = create((set) => {
   let storedUser = null;
 
   if (typeof window !== "undefined") {
-    const base64User = localStorage.getItem("encryptedUser");
-    const base64Iv = localStorage.getItem("ivUser");
-    const encryptedUser = base64User && base64ToArrayBuffer(base64User);
-    const iv = base64Iv && base64ToArrayBuffer(base64Iv);
-    getKey().then((key) => {
-      if (encryptedUser && iv && key) {
-        decryptData(encryptedUser, iv, key).then((user) => {
-          storedUser = user;
-          console.log("Stored user:  ", user)
-        });
-      }
-    });
-
-    
-    // storedUser = localStorage.getItem("user");
+    storedUser = localStorage.getItem("user");
   }
 
   return {
-    user: storedUser || null,
-    updateUser: (user) => set({ user }),
-    setUser: async (newUser) => {
-      set({ token: newUser });
+    user: storedUser ? JSON.parse(storedUser) : null,
+    setUser: (newUser) => {
+      set({ user: newUser });
       if (typeof window !== "undefined") {
-        const key = await getKey();
-        const { encrypted, iv } = await encryptData(newUser, key);
-        const base64EncryptedUser = arrayBufferToBase64(encrypted);
-        const base64Iv = arrayBufferToBase64(iv);
-        localStorage.setItem("encryptedUser", base64EncryptedUser);
-        localStorage.setItem("ivUser", base64Iv);
-        set({ user: newUser });
+        localStorage.setItem("user", JSON.stringify(newUser));
       }
     },
     removeUser: () => {
       set({ user: null });
       if (typeof window !== "undefined") {
-        localStorage.removeItem("encryptedUser");
-        localStorage.removeItem("ivUser");
+        localStorage.removeItem("user");
       }
     },
   };
