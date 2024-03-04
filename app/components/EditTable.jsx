732295b98b7077103ee0f537912c2f788c24c9f1
pvgmoon <pandreavanegasg@gmail.com>
2024-03-04 09:50:47 -0500

    Correccion estado

--- app/components/EditTable.jsx
+++ app/components/EditTable.jsx
@@ -923,20 +923,23 @@ export default function EditTable({
                                     onClick={() => setIsSelectDisabled(false)}
                                   >
                                     {column === "Code" && (
+                                      //Codigo del estado
                                       <div className="flex flex-row items-center">
-                                        <div
-                                          className={`w-2 h-2 rounded-full ${
-                                            row.state === "SHORT" ||
-                                            row.state === "ND"
-                                              ? "bg-danger"
-                                              : row.state === "PD" ||
-                                                row.state === "FULL"
-                                              ? "bg-green"
-                                              : row.state === "N/A"
-                                              ? "bg-primary-blue"
-                                              : "bg-gray-input"
-                                          } mr-2`}
-                                        />
+                                        {row[column] != "" && (
+                                          <div
+                                            className={`w-2 h-2 rounded-full ${
+                                              row.state === "SHORT" ||
+                                              row.state === "ND"
+                                                ? "bg-danger"
+                                                : row.state === "PD" ||
+                                                  row.state === "FULL"
+                                                ? "bg-green"
+                                                : row.state === "N/A"
+                                                ? "bg-primary-blue"
+                                                : "bg-gray-input"
+                                            } mr-2`}
+                                          />
+                                        )}
                                         <p>{row[column]}</p>
                                       </div>
                                     )}
