eb294d6c1494273cd0704ec61b2c3404c306090f
Santiago Arango <arangosanti05@gmail.com>
2024-03-07 15:31:29 -0500

    actualizacion para open day y close day

--- app/store/useWorkDateStore.jsx
+++ app/store/useWorkDateStore.jsx
@@ -12,6 +12,9 @@ const useWorkDateStore = create((set) => ({
     setStartDateByNet
   ) => {
     try {
+      // Agregar un tiempo de espera, ajusta el valor según tus necesidades
+      await new Promise(resolve => setTimeout(resolve, 1000)); // Espera 1 segundo (ajusta según sea necesario)
+
       const response = await axios.post(
         dateAvailable,
         { supplier: idSupplier },
@@ -21,12 +24,21 @@ const useWorkDateStore = create((set) => ({
           },
         }
       );
+
+      console.log("🚀 ~ useWorkDateStore ~ response:", response);
+
       setEndDateByNet(response.data.operation.principal[0].fecha);
       setStartDateByNet(response.data.operation.principal[0].fecha);
       set({ workDate: response.data.operation.principal[0].fecha });
+
+      // Devuelve una promesa resuelta
+      return Promise.resolve();
     } catch (error) {
       console.error("error setFetchWorkDate: ", error);
+      // Devuelve una promesa rechazada
+      return Promise.reject(error);
     }
   },
 }));
+
 export default useWorkDateStore;
