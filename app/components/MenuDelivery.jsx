12ea8a9c257fb51fc43f9483e415a3b168a4b6df
pvgmoon <pandreavanegasg@gmail.com>
2024-03-12 11:16:25 -0500

    Cambios estilos vista deliveries

--- app/components/MenuDelivery.jsx
+++ app/components/MenuDelivery.jsx
@@ -1,23 +1,5 @@
-import {
-  ArrowLeftOnRectangleIcon,
-  Bars3Icon,
-  XMarkIcon,
-  MoonIcon,
-  CircleStackIcon,
-  ChevronDownIcon,
-  ChevronUpIcon,
-  UserIcon,
-  ShoppingCartIcon,
-  CalendarIcon,
-  BuildingStorefrontIcon,
-  ClipboardIcon,
-  SunIcon,
-  TruckIcon,
-  ArrowRightIcon,
-} from "@heroicons/react/24/outline";
-import Link from "next/link";
-import { useRouter } from "next/navigation";
-import { usePathname } from "next/navigation";
+import { ArrowRightIcon } from "@heroicons/react/24/outline";
+import { TruckIcon } from "@heroicons/react/24/solid";
 import { Fragment, useLayoutEffect, useState, useEffect } from "react";
 import useUserStore from "../store/useUserStore";
 import { Dialog, Transition } from "@headlessui/react";
@@ -29,10 +11,7 @@ import { closeDay, openDay } from "../config/urls.config";
 import useWorkDateStore from "../store/useWorkDateStore";
 import Swal from "sweetalert2";
 
-
 const MenuDelivery = ({ open, setOpen }) => {
-  const pathname = usePathname();
-
   return (
     <div>
       <Transition.Root show={open} as={Fragment}>
@@ -62,51 +41,131 @@ const MenuDelivery = ({ open, setOpen }) => {
             leaveFrom="translate-x-0"
             leaveTo="translate-x-full"
           >
-            <div className="fixed inset-y-0 right-0 flex max-w-full">
-              <Dialog.Panel className="pointer-events-auto relative w-screen max-w-[850px] max-h-full">
+            <div className="fixed inset-y-0 right-0 flex max-w-full text-dark-blue">
+              <Dialog.Panel className="pointer-events-auto relative w-screen max-w-[750px] max-h-full">
                 <div className="absolute left-0 top-0 flex pl-2 pt-4  mr-2">
                   <button
                     type="button"
-                    className="relative rounded-md text-white hover:text-white focus:outline-none focus:ring-2 focus:ring-white"
+                    className="relative rounded-md focus:outline-none"
                     onClick={() => setOpen(false)}
                   >
                     <span className="absolute -inset-2.5" />
                     <span className="sr-only">Close panel</span>
-                    <ArrowRightIcon className="h-6 w-6 text-black ml-2" aria-hidden="true" />
+                    <ArrowRightIcon
+                      className="h-6 w-6  ml-2"
+                      aria-hidden="true"
+                    />
                   </button>
                 </div>
-                <div className="flex h-full flex-col bg-white py-6 font-poppins">
-                  <div className="px-4 sm:px-6">
-                    <Dialog.Title className="text-base font-semibold leading-6 text-white">
-                      <h1 className="mt-8 text-center text-black text-xl mb-1"><strong>Detail Delivery</strong></h1>
-                      <p className="text-black"><strong>Evidence:</strong></p>
-                      <div className="bg-gray-500 py-3 px-5 rounded-2xl flex items-center">
-                        <Image
-                          src={grownetLogo}
-                          alt="Grownet Logo"
-                          width={250}
-                          height={250}
-                        />
-                      </div>
+                <div className="flex h-full flex-col bg-white py-6 font-poppins px-3 items-center">
+                  <div className="px-10">
+                    <Dialog.Title className="text-base font-semibold leading-6">
+                      <h1 className="mt-3 text-center text-xl mb-5 font-bold  flex items-center justify-center">
+                        <span className="mr-1 flex items-center text-primary-blue">
+                          <TruckIcon className="h-7 w-7 mr-1" />
+                          Route 0:
+                        </span>
+                        Field to Fork
+                      </h1>
+                      <img
+                        className="rounded-lg h-[310px]"
+                        src="https://images.pexels.com/photos/442969/pexels-photo-442969.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                        alt="Grownet Logo"
+                      />
                     </Dialog.Title>
                   </div>
-                  <div className="grid grid-cols-2 gap-3 items-center text-start mt-5 mx-10">
-                    <p className="text-black"><strong>Account Name:</strong> Boba</p>
-                    <p className="text-black"><strong>Invoice Number:</strong> 1111</p>
-                    <p className="text-black"><strong>Account Number:</strong> REST100</p>
-                    <p className="text-black"><strong>Invoice Date:</strong> 111111</p>
-                    <p className="text-black"><strong>Round:</strong> 500</p>
-                    <p className="text-black"><strong>Drop:</strong> 3</p>
-                    <p className="text-black"><strong>Driver:</strong> Test</p>
-                    <p className="text-black"><strong>Delivery time:</strong> 05:00:00</p>
-                    <p className="text-black"><strong>Post Code:</strong> Test</p>
-                    <p className="text-black"><strong>Address:</strong> Cl test avenue</p>
-                    <p className="text-black"><strong>Crates:</strong> Yes</p>
-                    <p className="text-black"><strong>Vip:</strong> Yes</p>
-                    <p className="text-black"><strong>Group:</strong> UFC</p>
-                    <p className="text-black"><strong>Delivery Window:</strong> 12:00:00 - 13:00:00</p>
-                    <p className="text-black"><strong>Special Instructions:</strong> No special instructuions</p>
+                  <div className="grid grid-cols-2 gap-2 mt-6 mx-10">
+                    <p>
+                      <strong>
+                        <span className="mr-2 text-primary-blue">•</span>Account
+                        Name:
+                      </strong>{" "}
+                      Boba
+                    </p>
+                    <p>
+                      <strong>
+                        <span className="mr-2 text-primary-blue">•</span>Invoice
+                        Number:
+                      </strong>{" "}
+                      1111
+                    </p>
+                    <p>
+                      <strong>
+                        <span className="mr-2 text-primary-blue">•</span>Account
+                        Number:
+                      </strong>{" "}
+                      REST100
+                    </p>
+                    <p>
+                      <strong>
+                        <span className="mr-2 text-primary-blue">•</span>Invoice
+                        Date:
+                      </strong>{" "}
+                      111111
+                    </p>
+                    <p>
+                      <strong>
+                        <span className="mr-2 text-primary-blue">•</span>Round:
+                      </strong>{" "}
+                      500
+                    </p>
+                    <p>
+                      <strong>
+                        <span className="mr-2 text-primary-blue">•</span>Drop:
+                      </strong>{" "}
+                      3
+                    </p>
+                    <p>
+                      <strong>
+                        <span className="mr-2 text-primary-blue">•</span>Driver:
+                      </strong>{" "}
+                      Test
+                    </p>
+                    <p>
+                      <strong>
+                        <span className="mr-2 text-primary-blue">•</span>
+                        Delivery time:
+                      </strong>{" "}
+                      05:00:00
+                    </p>
+                    <p>
+                      <strong>
+                        <span className="mr-2 text-primary-blue">•</span>Post
+                        Code:
+                      </strong>{" "}
+                      Test
+                    </p>
+                    <p>
+                      <strong>
+                        <span className="mr-2 text-primary-blue">•</span>
+                        Address:
+                      </strong>{" "}
+                      Cl test avenue
+                    </p>
+                    <p>
+                      <strong>
+                        <span className="mr-2 text-primary-blue">•</span>Crates:
+                      </strong>{" "}
+                      Yes
+                    </p>
+                    <p>
+                      <strong>
+                        <span className="mr-2 text-primary-blue">•</span>Group:
+                      </strong>{" "}
+                      UFC
+                    </p>
+                    <p>
+                      <strong>
+                        <span className="mr-2 text-primary-blue">•</span>
+                        Delivery Window:
+                      </strong>{" "}
+                      12:00:00 - 13:00:00
+                    </p>
                   </div>
+                  <p className="bg-light-blue p-3 mt-2 rounded-lg w-[90%]">
+                    <strong>Special Instructions:</strong> Special Instructions
+                    for the order to grownet
+                  </p>
                 </div>
               </Dialog.Panel>
             </div>
