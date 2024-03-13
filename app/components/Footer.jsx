30e5de895a82413e11ba57be5285cbe769e25562
pvgmoon <pandreavanegasg@gmail.com>
2024-03-13 13:08:04 -0500

    Coreccion color footer

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -16,11 +16,11 @@ export default function Footer() {
               priority={true}
             />
           </Link>
-          <p className="text-sm text-gray-300 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 ml-5">
+          <p className="text-sm text-white sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 ml-5">
             © 2023 Grownet —
             <a
               href="https://twitter.com/knyttneve"
-              className="text-gray-400 ml-1"
+              className="ml-1"
               rel="noopener noreferrer"
               target="_blank"
             >
@@ -30,7 +30,7 @@ export default function Footer() {
         </div>
 
         <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
-          <a className="text-gray-300">
+          <a className="text-white">
             <svg
               fill="currentColor"
               strokeWidth="2"
@@ -42,7 +42,7 @@ export default function Footer() {
               <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
             </svg>
           </a>
-          <a className="ml-3 text-gray-300">
+          <a className="ml-3 text-white">
             <svg
               fill="currentColor"
               strokeWidth="2"
@@ -54,7 +54,7 @@ export default function Footer() {
               <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
             </svg>
           </a>
-          <a className="ml-3 text-gray-300">
+          <a className="ml-3 text-white">
             <svg
               fill="none"
               stroke="currentColor"
@@ -68,7 +68,7 @@ export default function Footer() {
               <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
             </svg>
           </a>
-          <a className="ml-3 text-gray-300">
+          <a className="ml-3 text-white">
             <svg
               fill="currentColor"
               stroke="currentColor"
