3163a3830bc3fe8a4aab69dacef421165a2ecc4a
Santiago Arango <arangosanti05@gmail.com>
2024-03-13 16:59:06 -0500

    se agrega flagshort a presentations

--- app/config/urls.config.js
+++ app/config/urls.config.js
@@ -1,6 +1,6 @@
 //BASE URL
-const BASE_URL = "https://api.grownetapp.com/grownet/api/";
-//const BASE_URL = "http://3.139.16.141/api/";
+// const BASE_URL = "https://api.grownetapp.com/grownet/api/";
+const BASE_URL = "http://3.139.16.141/api/";
 
 // Login
 export const loginUrl = `${BASE_URL}login/login`;
@@ -118,3 +118,4 @@ export const typesUrl = `${BASE_URL}types/all`;
 // Shorts
 export const productShort = `${BASE_URL}products/short`;
 export const categoriesShort = `${BASE_URL}categories/short`;
+export const typeShort = `${BASE_URL}suppliers/short/create`;
