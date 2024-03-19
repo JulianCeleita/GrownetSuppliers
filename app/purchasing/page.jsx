f96e4467bb2e0c10dd804b6de7160b3172538918
Santiago Arango <arangosanti05@gmail.com>
2024-03-19 12:09:00 -0500

    funcional la tabla para mandar informacion con filtro de categories

--- app/purchasing/page.jsx
+++ app/purchasing/page.jsx
@@ -1,5 +1,6 @@
 "use client";
 import {
+  ArrowRightCircleIcon,
   MinusCircleIcon,
   NoSymbolIcon,
   PencilSquareIcon,
@@ -9,7 +10,7 @@ import {
 import axios from "axios";
 import { useEffect, useState } from "react";
 import EditPresentation from "../../app/components/EditPresentation";
-import { deletePresentationUrl, purchasingUrl, wholesalersUrl } from "../../app/config/urls.config";
+import { deletePresentationUrl, purchasingCreate, purchasingUrl, wholesalersUrl } from "../../app/config/urls.config";
 import useTokenStore from "../../app/store/useTokenStore";
 import ModalDelete from "../components/ModalDelete";
 import Layout from "../layoutS";
@@ -19,6 +20,8 @@ import CreateProduct from "../components/CreateProduct";
 import AutomaticShort from "../components/AutomaticShort";
 import DatePicker from "react-datepicker";
 import useWorkDateStore from "../store/useWorkDateStore";
+import ModalSuccessfull from "../components/ModalSuccessfull";
+import ModalOrderError from "../components/ModalOrderError";
 
 export const fetchOrderWholesaler = (start, end, token, setOrdersWholesaler, setIsLoading) => {
   if (!end || !start || start === new Date()) {
@@ -85,6 +88,10 @@ function Purchasing() {
   const [wholesalerList, setWholesalerList] = useState([]);
   const [selectedWholesalers, setSelectedWholesalers] = useState(Array(filteredOrdersWholesaler.length).fill(null));
   const [selectedCategory, setSelectedCategory] = useState("");
+  const [showSuccessModal, setShowSuccessModal] = useState(false);
+  const [showErrorModal, setShowErrorModal] = useState(false);
+  const [messageError, setMessageError] = useState("");
+  const [isSendOrderDisabled, setIsSendOrderDisabled] = useState(true);
 
   const defaultDate = new Date();
 
@@ -143,6 +150,25 @@ function Purchasing() {
     setFilteredOrdersWholesaler(filteredOrdersByCategory);
   }, [ordersWholesaler, selectedStatus, selectedCategory]);
 
+  useEffect(() => {
+    const updatedOrders = ordersWholesaler.map((order, index) => ({
+      ...order,
+      wholesaler_id: selectedWholesalers[index]?.value || order.wholesaler_id,
+      quantity: editableRows[index]?.quantity || order.quantity,
+      cost: editableRows[index]?.cost || order.cost,
+      note: editableRows[index]?.notes || order.note,
+    }));
+    setFilteredOrdersWholesaler(updatedOrders);
+  }, [ordersWholesaler, editableRows]);
+
+  const checkIfAnyProductHasQuantity = () => {
+    return filteredOrdersWholesaler.some(order => order.quantity > 0);
+  };
+
+  useEffect(() => {
+    setIsSendOrderDisabled(!checkIfAnyProductHasQuantity());
+  }, [filteredOrdersWholesaler]);
+
   const handleEditField = (key, rowIndex, e) => {
     const value = e.target.value;
     const updatedRows = { ...editableRows };
@@ -151,6 +177,7 @@ function Purchasing() {
     }
     updatedRows[rowIndex][key] = value;
     setEditableRows(updatedRows);
+    console.log("Editable Rows:", updatedRows);
   };
 
   const handleSort = (column) => {
@@ -164,6 +191,46 @@ function Purchasing() {
 
   const uniqueCategories = [...new Set(ordersWholesaler.map(order => order.category_name))];
 
+  const sendOrder = async () => {
+    try {
+      const ordersToSend = filteredOrdersWholesaler.filter(order => order.quantity > 0);
+      console.log("🚀 ~ sendOrder ~ ordersToSend:", ordersToSend)
+
+      const sendData = {
+        orders_wholesaler: ordersToSend.map((order, index) => ({
+          presentation_code: order.presentation_code,
+          wholesaler_id: selectedWholesalers[index] ? selectedWholesalers[index].value : null,
+          date_delivery: workDate,
+          note: order.note,
+          cost: order.cost,
+          purchasing_qty: order.quantity
+        }))
+      };
+      console.log("🚀 ~ sendOrder ~ sendData:", sendData)
+
+      const response = await axios.post(purchasingCreate, sendData, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      console.log(response)
+
+      if (response.status === 200) {
+        fetchOrderWholesaler(startDate, endDate, token, setOrdersWholesaler, setIsLoading)
+        setSelectedWholesalers(Array(filteredOrdersWholesaler.length).fill(null));
+        setEditableRows({});
+        setFilteredOrdersWholesaler([]);
+        setShowSuccessModal(true)
+      }
+
+    } catch (error) {
+      setMessageError(error.response.data.message);
+      setShowErrorModal(true);
+      console.error(error);
+    }
+  };
+
+
   return (
     <Layout>
       <div>
@@ -174,12 +241,13 @@ function Purchasing() {
 
           <div className="flex gap-4">
             <button
-              className="flex bg-green py-3 px-4 rounded-lg text-white font-medium hover:scale-110 transition-all"
+              className={`flex bg-green py-3 px-4 rounded-lg text-white font-medium hover:scale-110 transition-all ${isSendOrderDisabled ? 'bg-gray-400 cursor-not-allowed' : ''}`}
               type="button"
-              onClick={() => setShowNewPresentations(true)}
+              onClick={sendOrder}
+              disabled={isSendOrderDisabled}
             >
-              <PlusCircleIcon className="h-6 w-6 mr-2 font-bold" />
-              New Purchasing
+              <ArrowRightCircleIcon className="h-6 w-6 mr-2 font-bold" />
+              Send Purchasing
             </button>
           </div>
         </div>
@@ -281,10 +349,10 @@ function Purchasing() {
                   onClick={() => handleSort("description")}>
                   Description
                 </th>
-                <th className="p-4 cursor-pointer hover:bg-gray-100 transition-all"
+                {/* <th className="p-4 cursor-pointer hover:bg-gray-100 transition-all"
                   onClick={() => handleSort("soh")}>
                   SOH
-                </th>
+                </th> */}
                 <th className="p-4 cursor-pointer hover:bg-gray-100 transition-all"
                   onClick={() => handleSort("requisition")}>
                   Requisition
@@ -320,82 +388,89 @@ function Purchasing() {
               </tr>
             </thead>
             <tbody>
-              {filteredOrdersWholesaler?.map((order, index) => (
-                <tr className="text-dark-blue border-b-2 border-stone-100">
-                  <td className="py-4 pl-3">{order.presentation_code}</td>
-                  <td className="py-4">
-                    <Select
-                      value={selectedWholesalers[index]}
-                      onChange={(selectedOption) => {
-                        const newSelectedWholesalers = [...selectedWholesalers];
-                        newSelectedWholesalers[index] = selectedOption;
-                        setSelectedWholesalers(newSelectedWholesalers);
-                      }}
-                      options={wholesalerList.map(wholesaler => ({
-                        value: wholesaler.id,
-                        label: wholesaler.name
-                      }))}
-                      menuPortalTarget={document.body}
-                      styles={{
-                        control: (provided) => ({
-                          ...provided,
-                          border: "none",
-                          boxShadow: "none",
-                          backgroundColor: "transparent",
-                        }),
-                        menu: (provided) => ({
-                          ...provided,
-                          width: "33em",
-                        }),
-                        singleValue: (provided, state) => ({
-                          ...provided,
-                          color: "#04444F",
-                        }),
-                        dropdownIndicator: (provided) => ({
-                          ...provided,
-                          display: "none",
-                        }),
-                        indicatorSeparator: (provided) => ({
-                          ...provided,
-                          display: "none",
-                        }),
-                      }}
-                    />
-                  </td>
-                  <td className="py-4">{order.product_name} - {order.presentation_name}</td>
-                  <td className="py-4">{order.soh}</td>
-                  <td className="py-4">{order.requisitions}</td>
-                  <td className="py-4">{order.future_requisitions}</td>
-                  <td className="py-4">{order.short}</td>
-                  <td className="py-4">{order.ordered}</td>
-                  <td className="py-4">
-                    <input
-                      type="number"
-                      value={editableRows[index]?.quantity || order.quantity}
-                      onChange={(e) => handleEditField("quantity", index, e)}
-                      className="w-16 px-2 py-1 rounded-md border border-gray-300 text-sm"
-                    />
-                  </td>
-                  <td className="py-4">
-                    <input
-                      type="number"
-                      step="0.01"
-                      value={editableRows[index]?.cost || order.cost}
-                      onChange={(e) => handleEditField("cost", index, e)}
-                      className="w-16 px-2 py-1 rounded-md border border-gray-300 text-sm"
-                    />
-                  </td>
-                  <td className="py-4">{order.totalCost}</td>
-                  <td className="py-4">
-                    <input
-                      type="text"
-                      value={editableRows[index]?.note || order.note}
-                      onChange={(e) => handleEditField("notes", index, e)}
-                      className="w-32 px-2 py-1 rounded-md border border-gray-300 text-sm"
-                    />
-                  </td>
-                </tr>
-              ))}
+              {filteredOrdersWholesaler?.map((order, index) => {
+                const quantity = editableRows[index]?.quantity || order.quantity;
+                const cost = editableRows[index]?.cost || order.cost;
+                const totalCost = isNaN(quantity * cost) ? 0 : quantity * cost;
+                return (
+                  <tr className="text-dark-blue border-b-2 border-stone-100">
+                    <td className="py-4 pl-3">{order.presentation_code}</td>
+                    <td className="py-4">
+                      <Select
+                        value={selectedWholesalers[index]}
+                        onChange={(selectedOption) => {
+                          const newSelectedWholesalers = [...selectedWholesalers];
+                          newSelectedWholesalers[index] = selectedOption;
+                          setSelectedWholesalers(newSelectedWholesalers);
+                        }}
+                        options={wholesalerList.map(wholesaler => ({
+                          value: wholesaler.id,
+                          label: wholesaler.name
+                        }))}
+                        menuPortalTarget={document.body}
+                        styles={{
+                          control: (provided) => ({
+                            ...provided,
+                            border: "none",
+                            boxShadow: "none",
+                            backgroundColor: "transparent",
+                          }),
+                          menu: (provided) => ({
+                            ...provided,
+                            width: "33em",
+                          }),
+                          singleValue: (provided, state) => ({
+                            ...provided,
+                            color: "#04444F",
+                          }),
+                          dropdownIndicator: (provided) => ({
+                            ...provided,
+                            display: "none",
+                          }),
+                          indicatorSeparator: (provided) => ({
+                            ...provided,
+                            display: "none",
+                          }),
+                        }}
+                      />
+                    </td>
+                    <td className="py-4">{order.product_name} - {order.presentation_name}</td>
+                    {/* <td className="py-4">{order.soh}</td> */}
+                    <td className="py-4">{order.requisitions}</td>
+                    <td className="py-4">{order.future_requisitions}</td>
+                    <td className="py-4">{order.short}</td>
+                    <td className="py-4">{order.ordered}</td>
+                    <td className="py-4">
+                      <input
+                        type="number"
+                        value={editableRows[index]?.quantity || order.quantity}
+                        onChange={(e) => handleEditField("quantity", index, e)}
+                        className="w-16 px-2 py-1 rounded-md border-none text-sm"
+                        style={{ WebkitAppearance: "none", MozAppearance: "textfield" }}
+                      />
+                    </td>
+                    <td className="py-4">
+                      <input
+                        type="number"
+                        step="0.01"
+                        value={editableRows[index]?.cost || order.cost}
+                        onChange={(e) => handleEditField("cost", index, e)}
+                        className="w-16 px-2 py-1 rounded-md border-none text-sm"
+                        style={{ WebkitAppearance: "none", MozAppearance: "textfield" }}
+                      />
+                    </td>
+                    <td className="py-4">{totalCost}</td>
+                    <td className="py-4">
+                      <input
+                        type="text"
+                        value={editableRows[index]?.notes || order.note}
+                        onChange={(e) => handleEditField("notes", index, e)}
+                        className="w-32 px-2 py-1 rounded-md border-none text-sm"
+                      />
+                    </td>
+                  </tr>
+                )
+              })}
             </tbody>
           </table>
         </div>
@@ -404,6 +479,19 @@ function Purchasing() {
             <div className="loader"></div>
           </div>
         )}
+        <ModalSuccessfull
+          isvisible={showSuccessModal}
+          onClose={() => setShowSuccessModal(false)}
+          title="Congratulations"
+          text="Order sended successfully"
+          button=" Close"
+        />
+        <ModalOrderError
+          isvisible={showErrorModal}
+          onClose={() => setShowErrorModal(false)}
+          error={messageError}
+          title={"Error sending order"}
+        />
       </div>
     </Layout>
   );
