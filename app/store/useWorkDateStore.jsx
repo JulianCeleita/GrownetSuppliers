4a5fd6f1913479c84030d8342a7a1b4c8dca6cc0
Santiago Arango <arangosanti05@gmail.com>
2024-03-27 11:46:34 -0500

    correccion en fechas y se agrega para ordenar en asc y desc

--- app/store/useWorkDateStore.jsx
+++ app/store/useWorkDateStore.jsx
@@ -21,7 +21,6 @@ const useWorkDateStore = create((set) => ({
           },
         }
       );
-      console.log("🚀 ~ useWorkDateStore ~ response.data.operation.principal[0]:", response.data.operation.principal[0])
       setEndDateByNet(response.data.operation.principal[0].fecha);
       setStartDateByNet(response.data.operation.principal[0].fecha);
       set({ workDate: response.data.operation.principal[0].fecha });
